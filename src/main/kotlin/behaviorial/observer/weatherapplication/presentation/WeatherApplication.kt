86733846b0d3f8be289a2d4b0baed60c5057c8cd
Anmol <sahi.anmool@gmail.com>
2024-01-02 21:53:20 +0530

    initial commit - add ForecastDisplay and StatisticsDisplay

--- src/main/kotlin/behaviorial/observer/weatherapplication/presentation/WeatherApplication.kt
+++ src/main/kotlin/behaviorial/observer/weatherapplication/presentation/WeatherApplication.kt
@@ -2,20 +2,16 @@ package behaviorial.observer.weatherapplication.presentation
 
 import behaviorial.observer.weatherapplication.data.WeatherData
 import behaviorial.observer.weatherapplication.presentation.displays.CurrentConditionsDisplay
+import behaviorial.observer.weatherapplication.presentation.displays.ForecastDisplay
+import behaviorial.observer.weatherapplication.presentation.displays.StatisticsDisplay
 
 fun main() {
     val weatherData = WeatherData()
     val currentConditionsDisplay = CurrentConditionsDisplay(weatherData)
+    val forecastDisplay = ForecastDisplay(weatherData)
+    val statisticsDisplay = StatisticsDisplay(weatherData)
 
-    weatherData.setMeasurements(
-        temperature = 98.0F, humidity = 56.0F, pressure = 6.7F
-    )
-
-    weatherData.setMeasurements(
-        temperature = 60.0F, humidity = 58.0F, pressure = .7F
-    )
-
-    weatherData.setMeasurements(
-        temperature = 34.0F, humidity = 78.0F, pressure = 6.43F
-    )
+    weatherData.setMeasurements(80.0F, 65.0F, 30.4F)
+    weatherData.setMeasurements(82.0F, 70.0F, 29.2F)
+    weatherData.setMeasurements(78.0F, 90.0F, 29.2F)
 }
--- src/main/kotlin/behaviorial/observer/weatherapplication/presentation/displays/StatisticsDisplay.kt
+++ src/main/kotlin/behaviorial/observer/weatherapplication/presentation/displays/StatisticsDisplay.kt
@@ -1,4 +1,39 @@
 package behaviorial.observer.weatherapplication.presentation.displays
 
-class StatisticsDisplay {
+import behaviorial.observer.observer.Observer
+import behaviorial.observer.weatherapplication.data.WeatherData
+import behaviorial.observer.weatherapplication.presentation.displays.contract.DisplayElement
+
+class StatisticsDisplay(
+    private val weatherData: WeatherData
+) : Observer, DisplayElement {
+    private var maxTemp = 0.0f
+    private var minTemp = 200f
+    private var tempSum = 0.0f
+    private var numReadings = 0
+
+    init {
+        weatherData.addObserver(this)
+    }
+
+    override fun update() {
+        tempSum += weatherData.temperature;
+        numReadings++
+
+        if (weatherData.temperature > maxTemp) {
+            maxTemp = weatherData.temperature
+        }
+
+        if (weatherData.temperature < minTemp) {
+            minTemp = weatherData.temperature
+        }
+        display()
+    }
+
+    override fun display() {
+        println(
+            "Avg/Max/Min temperature = " + (tempSum / numReadings)
+                    + "/" + maxTemp + "/" + minTemp
+        )
+    }
 }
